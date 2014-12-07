fd8d1d2c97cb1adcfd787eeecf268c473ac9dde3
Stefan Ziegler <edi.gonzales@gmail.com>
2014-12-07 17:26:27 +0100

    db-sachen aufgeräumt, da jetzt pgmodeler

--- db/01_schema.sql
+++
@@ -1,4 +0,0 @@
-CREATE SCHEMA metadb_ng
-  AUTHORIZATION stefan;
-GRANT ALL ON SCHEMA metadb_ng TO stefan;
-GRANT USAGE ON SCHEMA metadb_ng TO mspublic;
--- db/03_insert.sql
+++ db/03_insert.sql
@@ -1,9 +1,10 @@
 INSERT INTO metadb_ng."type" (code, code_txt) VALUES (0, 'undefined');
-INSERT INTO metadb_ng."type" (code, code_txt) VALUES (1, 'dataset');
-INSERT INTO metadb_ng."type" (code, code_txt) VALUES (100, 'wms');  
-INSERT INTO metadb_ng."type" (code, code_txt) VALUES (101, 'wmts');  
-INSERT INTO metadb_ng."type" (code, code_txt) VALUES (102, 'wfs');  
-INSERT INTO metadb_ng."type" (code, code_txt) VALUES (200, 'postgres');  
+INSERT INTO metadb_ng."type" (code, code_txt) VALUES (100, 'dataset');
+INSERT INTO metadb_ng."type" (code, code_txt) VALUES (200, 'wms');  
+INSERT INTO metadb_ng."type" (code, code_txt) VALUES (210, 'wmts');  
+INSERT INTO metadb_ng."type" (code, code_txt) VALUES (220, 'wfs');  
+INSERT INTO metadb_ng."type" (code, code_txt) VALUES (230, 'wps');  
+INSERT INTO metadb_ng."type" (code, code_txt) VALUES (500, 'postgres');  
 
 
 INSERT INTO metadb_ng.metadb (identifier, title, abstract, "type") VALUES ('fd6884e2-385c-463a-a59a-447560bd187c', 'Amtliche Vermessung', 'Amtliche Vermessung des Kantons Solothurn', 0);
